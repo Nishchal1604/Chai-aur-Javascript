809358f7c7856084a06489e14bfbb133e5320c86
NISHCHAL GUPTA <98217664+Nishchal1604@users.noreply.github.com>
2024-06-01 12:33:05 +0000

    saved

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,38 +1,52 @@
-// singleton
-// Object.create
+//singleton
+//constructor with singleton...........literals without singleton
 
-// object literals
 
-const mySym = Symbol("key1")
+// object literals   -->>>to declare literals
+//Object.create
+
+const mySym = Symbol("Key1")
 
 
 const JsUser = {
     name: "Hitesh",
     "full name": "Hitesh Choudhary",
-    [mySym]: "mykey1",
+    mySym: "mykey1",
+    [mySym]: "mykey1",  // to represent a symbol as a key--->>>very important for interview
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
+console.log(JsUser.fullname)
+console.log(JsUser.email)
+console.log(JsUser["email"])
+console.log(JsUser["full name"])
+console.log(JsUser.mySym) 
+console.log(typeof JsUser.mySym)
+console.log(JsUser[mySym])
+console.log(typeof JsUser[mySym])
 
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
-// console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+JsUser.email= "dfhao@gmail.com"  //this way we can change the value of any key
 
-JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
+//to avoid any change in object keys later on, we can use FREEZE
+
+Object.freeze(JsUser)
 JsUser.email = "hitesh@microsoft.com"
-// console.log(JsUser);
+console.log(JsUser);
 
-JsUser.greeting = function(){
-    console.log("Hello JS user");
-}
-JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
-}
 
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+
+//treat functions just  like a variable
+
+
+// JsUser.greeting = function(){
+//     console.log("Hello JS user");
+// }
+// JsUser.greetingTwo = function(){
+//     console.log(`Hello JS user, ${this.name}`);
+// }
+
+// console.log(JsUser.greeting());
+// console.log(JsUser.greetingTwo());  
