5fea0c850511c4bfb857c95efbfb12cd833e36df
NISHCHAL GUPTA <98217664+Nishchal1604@users.noreply.github.com>
2024-06-01 11:06:13 +0000

    container configure done

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -56,4 +56,19 @@ let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3));   //can't use from why????
+
+/*
+The use of Array.of is indeed a correct and concise way to create an array from individual variables. The Array.of method creates a new Array instance with a variable number of arguments, regardless of the number or type of the arguments
+
+if you want to use Array.from in above case, you need to provide an iterable or an array-like object. Since score1, score2, and score3 are individual variables, they are not inherently iterable together.
+ie:
+let str = "hello";
+let arrFromStr = Array.from(str);
+console.log(arrFromStr); // Output: ['h', 'e', 'l', 'l', 'o']
+
+
+let set = new Set([1, 2, 3]);
+let arrFromSet = Array.from(set);
+console.log(arrFromSet); // Output: [1, 2, 3]
+*/
