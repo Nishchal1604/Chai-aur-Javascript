1865619a624fa0de02d08ed1e30c86588ba0432c
NISHCHAL GUPTA <98217664+Nishchal1604@users.noreply.github.com>
2024-07-09 17:33:21 +0000

    container configure done

---
+++ 01_basics/05_string.js
@@ -0,0 +1,8 @@
+const name = "hitesh"
+const repoCount = 50
+
+console.log(name + repoCount + " Value")
+
+//String Interpolation
+console.log(`Hello, my name is ${name} and my repo count is ${repoCount}`);
+
