1734189abf1eae483b001ab1968ff4530723ed69
NISHCHAL GUPTA <98217664+Nishchal1604@users.noreply.github.com>
2024-07-09 11:07:50 +0000

    container configure done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,75 @@
-let score = "38"
+// let score = 38
+// console.log(typeof score)
+// console.log(typeof (score))
 
-console.log(typeof score)
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
+
+// let score = "38"
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+
+
+
+
+// let score = "38abc"   //this is not a number but still we are convertinf it to a number
+// console.log(typeof score)
+// let valueInNumber = Number(score)  // 'n' should always be capital
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)   // result comes out to be NaN which means not a number we are forcefully converting it to Number
+
+
+
+// let score = null
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// console.log(score)
+
+// let score = undefined
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(score)
+// console.log(valueInNumber)
+
+
+
+// let score = true
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(score)
+// console.log(valueInNumber)
+
+
+
+// let score = "hitesh"
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(score)
+// console.log(valueInNumber) // "hitesh is a string thus it could not be completed into number so result we get is NaN"
+
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+// let isLoggedIn = 1
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+// ""--> false
+// "nishchal"--> true
+// 0--> false
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
