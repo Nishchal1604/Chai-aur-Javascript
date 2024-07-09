32e02adf3d832d5a8338acf66891ec73fc42a512
NISHCHAL GUPTA <98217664+Nishchal1604@users.noreply.github.com>
2024-07-09 11:26:42 +0000

    container configure done

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,31 @@
+let value= 3
+let negvalue= -value
+
+console.log(negvalue)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);                      //this refers to power ^
+console.log(2/3);
+console.log(2%3);
+
+let str1= "Hello"
+let str2= "NIshchal"
+let str3= str1 + str2
+
+console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);                //122
+// console.log(1 + 2 + "2");                //32
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);                      //1
+// console.log(+"");                        //0
+
+
+
+    
